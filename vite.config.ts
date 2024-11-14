0875443caf5314cdbdeb420c1de381fa5472ac24
heiwa4126 <heiwa4126@gmail.com>
2024-11-14 14:30:52 +0900

    [WIP] update (@tomjs/vite-plugin-html)

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
+import html from "@tomjs/vite-plugin-html";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import cdn from "vite-plugin-cdn-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		cdn({
-			modules: [
-				"react",
-				"react-dom",
-				{
-					name: "cowsay",
-					var: "cowsay",
-					// path: "build/cowsay.umd.min.js",
-					// jsDelivrの場合はこれで終わり
-					// UNPKGの場合
-					prodUrl: "https://unpkg.com/{name}@{version}/{path}",
-					path: "build/cowsay.umd.js", //UNPKGにはminがないらしい
-				},
-			],
+		html({
+			cdn: {
+				modules: [
+					"react",
+					"react-dom",
+					{ name: "cowsay", var: "cowsay", file: ["build/cowsay.umd.min.js"] },
+				],
+			},
 		}),
+		// cdn({
+		// 	modules: [
+		// 		"react",
+		// 		"react-dom",
+		// 		{
+		// 			name: "cowsay",
+		// 			var: "cowsay",
+		// 			// path: "build/cowsay.umd.min.js",
+		// 			// jsDelivrの場合はこれで終わり
+		// 			// UNPKGの場合
+		// 			prodUrl: "https://unpkg.com/{name}@{version}/{path}",
+		// 			path: "build/cowsay.umd.js", //UNPKGにはminがないらしい
+		// 		},
+		// 	],
+		// }),
 	],
 });
